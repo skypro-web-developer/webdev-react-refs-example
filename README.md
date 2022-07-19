b345a6a4a438f0be77e2509e55219e3a436d6127
Mikhail Cheremuhin-Rerberg <mcheremukhin-rerberg@alfabank.ru>
2022-07-19 16:53:09 +0300

    feat(*): init repo

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# react-refs
+# React refs examples
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Player} from './examples/audio/player'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* Media example  */}
+        <Player />
+      </header>
+    </div>
+  );
+}
+
+export default App;
