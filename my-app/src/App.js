341a07ab1a51aac9972de49dfdc65e5fa4f8563b
XshadowE <nimro-dell@mail.ru>
2023-10-27 15:53:39 +0700

    react1

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Здесь скоро будет Skypro.Music
+      </header>
+    </div>
+  );
+}
+
+export default App;
